b5dce2675bd00ecb61934e72aa58b93f58df8072
Oğuzhan Yücedağ <oguzhanyucedagg@gmail.com>
2025-11-08 13:37:16 +0300

    .

---
+++ deneme.py
@@ -0,0 +1,35 @@
+# print("Oyuncu Kaydetme Programı...")
+
+# ad = input("Oyuncunun Adını Giriniz...:")
+# soyadını = input("Oyuncunun Soyadını Giriniz...:")
+# takım = input("Oyuncunun Takımını Giriniz...:")
+
+# bilgiler=[ad,soyadını,takım]
+# print("Oyuncunun bilgileri Kaydediliyor...")
+
+# print("Oyuncunun Adı {}\n Oyuncunun Soyadı {}\n Oyuncunun Takımı {}".format(bilgiler[0],bilgiler[1],bilgiler[2]))
+###############################################################################################################################
+
+
+# print("Delta hesaplama...")
+
+# a=int(input("a Değerini Giriniz..:"))
+# b=int(input("b Değerini Giriniz..:"))
+# c=int(input("c Değerini Giriniz..:"))
+
+# delta = b ** 2 - 4 * a * c
+
+# x1 = ( -b - delta ** 0.5) / ( 2 * a )
+# x2 = ( -b + delta ** 0.5) / ( 2 * a )
+
+# if(delta>0):
+#     print("iki kökü vardır...")
+#     print("birinci kök ...:",x1)
+#     print("iknci kök ...:",x2)
+# elif(delta<0):
+#     print("denklemin gerçel kökü yoktur...")
+# else:
+#     print("denklemin tek kökü vardır...")    
+#     print("kök..:",x1)
+
+###################################################################################
--- palindromSayiHesaplama.py
+++ palindromSayiHesaplama.py
@@ -1,14 +1,17 @@
 pSayı = 0
 pToplam = 0
-pBasamak = [0,0,0,0,0,0,0]
+pBasamak = [0,0,0,0,0,0,0]#pBasamak =[0,1,2,3]
 sayıyaKadar = int(input("Kaçıncı sayıya kadar palindrom sayıları saysın ve hesaplasın..:"))
 for i in range(1,sayıyaKadar +1):
     x = str(i)
     if x == x[::-1]:
-        pSayı += 1
-        pToplam += int(x)
-        pBasamak[len(x)-1] += 1
-        print("***")
+        pSayı += 1#pSayı=pSayı+1#pSayı++
+        pToplam += int(x)#pToplam =pToplam+1
+        pBasamak[len(x)-1] += 1#12=>'12'
+        
+        #print(len(x))
+        
+    
     
     
     
