15dece091120f687420358c850b52bb61dda2039
Oğuzhan Yücedağ <oguzhanyucedagg@gmail.com>
2025-11-12 18:11:05 +0300

    .

--- deneme.py
+++ deneme.py
@@ -11,25 +11,25 @@
 ###############################################################################################################################
 
 
-# print("Delta hesaplama...")
+print("Delta hesaplama...")
 
-# a=int(input("a Değerini Giriniz..:"))
-# b=int(input("b Değerini Giriniz..:"))
-# c=int(input("c Değerini Giriniz..:"))
+a=int(input("a Değerini Giriniz..:"))
+b=int(input("b Değerini Giriniz..:"))
+c=int(input("c Değerini Giriniz..:"))
 
-# delta = b ** 2 - 4 * a * c
+delta = b ** 2 - 4 * a * c
 
-# x1 = ( -b - delta ** 0.5) / ( 2 * a )
-# x2 = ( -b + delta ** 0.5) / ( 2 * a )
+x1 = ( -b - delta ** 0.5) / ( 2 * a )
+x2 = ( -b + delta ** 0.5) / ( 2 * a )
 
-# if(delta>0):
-#     print("iki kökü vardır...")
-#     print("birinci kök ...:",x1)
-#     print("iknci kök ...:",x2)
-# elif(delta<0):
-#     print("denklemin gerçel kökü yoktur...")
-# else:
-#     print("denklemin tek kökü vardır...")    
-#     print("kök..:",x1)
+if(delta>0):
+    print("iki kökü vardır...")
+    print("birinci kök ...:",x1)
+    print("iknci kök ...:",x2)
+elif(delta<0):
+    print("denklemin gerçel kökü yoktur...")
+else:
+    print("denklemin tek kökü vardır...")    
+    print("kök..:",x1)
 
 ###################################################################################
